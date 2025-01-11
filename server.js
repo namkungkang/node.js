ab5c7ed50e5074489081b6e49004b82905c23bf4
namkungkang <namkung0131@naver.com>
2025-01-11 16:28:43 +0900

    Revert "ejs"
    This reverts commit 69769885a03efe894eb7425e6a9ff7902d275179.

--- server.js
+++ server.js
@@ -10,3 +10,23 @@ app.get('/',(req,res)=>{
     res.send('반갑다')
 
 })
+const express = require('express')
+
+const app = express()
+
+app.listen(8080,()=> {
+    console.log('http://localhost:8080 에서 실행중')
+})
+
+app.get('/',(req,res)=>{
+    res.send('반갑다')
+
+})
+
+app.get('/news',(req,res)=> {
+        res.send('뉴스임')
+})
+
+app.get('/shop',(req,res)=> {
+    res.send('쇼핑사이트임')
+})
